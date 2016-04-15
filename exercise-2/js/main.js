62554a9c11118739b55cae7267ae22e9a7a974f6
C Nicholas Mao <mao@tacktechnologies.com>
2016-04-15 16:53:25 -0700

    huhuhuhuh

--- exercise-2/js/main.js
+++ exercise-2/js/main.js
@@ -4,63 +4,68 @@
 $(function() {
 	// Read in your data. On success, run the rest of your code
 	d3.csv('data/prepped_data.csv', function(error, data){
-	 	// Select SVG to work with, setting width and height (the vis <div> is defined in the index.html file)
+ 	// Select SVG to work with, setting width and height (the vis <div> is defined in the index.html file)
+	var svg = d3.select("vis")
+		.append('svg')
+		.attr('width', 600)
+		.attr('Length', 600);
+
+	// Margin: how much space to put in the SVG for axes/titles
+	var margin = {
+		left:70,
+		bottom:100,
+		top:50,
+		right:50,
+	};
+
+	// Height/width of the drawing area for symbols
+	var height = 600 - margin.bottom - margin.top;
+	var width = 600 - margin.left - margin.right;
+
+	// Append a 'g' element in which to place the circles,
+	var g = svg.append('g');
+
+	// shifted down and right from the top left corner using the margin values
+	g.attr('transform', 'translate(' + margin.left + ',' + margin.right + ')')
+
+	// Find minimum and maximum values, then define x (log) and y (linear) scales
 
 
-		// Margin: how much space to put in the SVG for axes/titles
-		var margin = {
-			left:70,
-			bottom:100,
-			top:50,
-			right:50,
-		};
+	// Perform a data-join for your data, creating circle element in your chart `g`
 
-		// Height/width of the drawing area for symbols
-		var height = 600 - margin.bottom - margin.top;
-		var width = 600 - margin.left - margin.right;
-
-		// Append a 'g' element in which to place the circles, 
-		// shifted down and right from the top left corner using the margin values
+	// Select all circles and bind data
 
 
-		// Find minimum and maximum values, then define x (log) and y (linear) scales
+	// Use the .enter() method to get your entering elements, and assign initial positions
 
 
-		// Perform a data-join for your data, creating circle element in your chart `g`
-
-		// Select all circles and bind data
+	// Use the .exit() and .remove() methods to remove elements that are no longer in the data
 
 
-		// Use the .enter() method to get your entering elements, and assign initial positions
+  	// Transition properties of the update selection
 
 
-		// Use the .exit() and .remove() methods to remove elements that are no longer in the data
-		
+	// Define x axis using d3.svg.axis(), assigning the scale as the xScale
 
-	  	// Transition properties of the update selection
-		
 
-		// Define x axis using d3.svg.axis(), assigning the scale as the xScale
-		
+	// Define y axis using d3.svg.axis(), assigning the scale as the yScale
 
-		// Define y axis using d3.svg.axis(), assigning the scale as the yScale
-		
 
-		// Append x axis to your SVG, specifying the 'transform' attribute to position it
-		
+	// Append x axis to your SVG, specifying the 'transform' attribute to position it
 
-		// Append y axis to your SVG, specifying the 'transform' attribute to position it
-		
 
-		// Append a text element to label your x axis, and position it appropriately
+	// Append y axis to your SVG, specifying the 'transform' attribute to position it
 
-		// Append a text element to label your y axis, and position it appropriately
-		
 
-		/* Using jQuery, select all circles and apply a tooltip
-		If you want to use bootstrap, here's a hint:
-		http://stackoverflow.com/questions/14697232/how-do-i-show-a-bootstrap-tooltip-with-an-svg-object
-		*/
+	// Append a text element to label your x axis, and position it appropriately
+
+	// Append a text element to label your y axis, and position it appropriately
+
+
+	/* Using jQuery, select all circles and apply a tooltip
+	If you want to use bootstrap, here's a hint:
+	http://stackoverflow.com/questions/14697232/how-do-i-show-a-bootstrap-tooltip-with-an-svg-object
+	*/
 
 
 	});
